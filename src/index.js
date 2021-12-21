5d9ea169fd561d9227e0a496dc0c70e3d66d4378
junghoonme <junghoon.dev@gmail.com>
2021-12-22 01:27:09 +0900

    Add: Vanilla To Do List using Pure Redux

--- src/index.js
+++ src/index.js
@@ -1,34 +1,60 @@
 import { createStore } from 'redux';
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-// initial state
-number.innerText = 0;
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
 
-// 고유한 액션 타입을 정의하는 방법
-const ADD = 'ADD';
-const MINUS = 'MINUS';
-
-// reducer: 액션을 받아서 새로운 상태를 만드는 함수
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state]; 
+    case DELETE_TODO:
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
-      return count;
+      return state;
   }
 };
 
-// store: 상태를 저장하는 객체
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-// dispatch: action을 reducer에 전달
-add.addEventListener('click', () => countStore.dispatch({ type: ADD }));
-minus.addEventListener('click', () => countStore.dispatch({ type: MINUS }));
+const addToDo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+};
 
-// subscribe: 변경된 상태를 처리하는 함수
-countStore.subscribe(() => (number.innerText = countStore.getState()));
+const deleteToDo = (e) => {
+  // html에서 받아온 값은 string이므로 숫자로 변환해야함
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch({ type: DELETE_TODO, id });
+};
+
+const paintToDos = () => {
+  ul.innerHTML = '';
+  const toDos = store.getState();
+  
+  toDos.forEach((toDo) => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+
+    btn.innerText = '❌';
+    btn.addEventListener('click', deleteToDo);
+
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  addToDo(toDo);
+  input.value = '';
+};
+
+form.addEventListener('submit', handleSubmit);
